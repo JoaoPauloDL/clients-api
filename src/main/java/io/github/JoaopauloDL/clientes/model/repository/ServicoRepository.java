f03940e741949ff198b3e28566c84430bbfebb67
João Paulo <joaopaulomayka@gmail.com>
2022-09-12 17:53:34 -0300

    commit da criacao da api de servicos prestados

--- src/main/java/io/github/JoaopauloDL/clientes/model/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.JoaopauloDL.clientes.model.repository;
-
-import io.github.JoaopauloDL.clientes.model.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
